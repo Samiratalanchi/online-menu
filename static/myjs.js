bf6fe6915961b695f4f24dccc1389943c0cd8a77
samira talanchi <samira73talanchi@gmail.com>
2021-10-25 11:49:09 +0330

    style page

--- static/myjs.js
+++ static/myjs.js
@@ -1,42 +1,52 @@
-const menu = [
-    {
-        "id": 01,
-        "name":"Margherita",
-        "price":"73,000",
-        "ingrediants":"tomato sauce, basil, mozarella",
-        "star": 2,
-        "imageAddress":"./static/img/margarita.png"
-    },
-    {
-        "id": 02,
-        "name":"Del Capo",
-        "price":"121,000",
-        "ingrediants":"tomato sauce, bakon, mozarella, calf steak",
-        "star": 5,
-        "imageAddress":"./static/img/delcapo.png"
-    },
-    {
-        "id": 03,
-        "name":"Miracle",
-        "price":"118,000",
-        "ingrediants":"tomato sauce, mushroom, peperoni, potato",
-        "star": 4,
-        "imageAddress":"./static/img/miracle.png"
-    },
-    {
-        "id": 04,
-        "name":"Indiana",
-        "price":"98,000",
-        "ingrediants":"tomato sauce, chiken breast, mozarella, chili bellpaper",
-        "star": 4,
-        "imageAddress":"./static/img/indiana.png"
-    },
-    {
-        "id": 05,
-        "name":"Royal",
-        "price":"116,000",
-        "ingrediants":"tomato sauce, mushroom, mozarella, bakon, salami",
-        "star": 5,
-        "imageAddress":"./static/img/royal.png"
-    },
-]
+
+
+window.onload = function() {
+    const menu = [
+        {
+            "id": 01,
+            "name":"Margherita",
+            "price":"73,000",
+            "ingrediants":"tomato sauce, basil, mozarella",
+            "star": 2,
+            "imageAddress":"./static/img/pizza/margarita.png"
+        },
+        {
+            "id": 02,
+            "name":"Del Capo",
+            "price":"121,000",
+            "ingrediants":"tomato sauce, bakon, mozarella, calf steak",
+            "star": 5,
+            "imageAddress":"./static/img/pizza/delcapo.png"
+        },
+        {
+            "id": 03,
+            "name":"Miracle",
+            "price":"118,000",
+            "ingrediants":"tomato sauce, mushroom, peperoni, potato",
+            "star": 4,
+            "imageAddress":"./static/img/pizza/miracle.png"
+        },
+        {
+            "id": 04,
+            "name":"Indiana",
+            "price":"98,000",
+            "ingrediants":"tomato sauce, chiken breast, mozarella, chili bellpaper",
+            "star": 4,
+            "imageAddress":"./static/img/pizza/indiana.png"
+        },
+        {
+            "id": 05,
+            "name":"Royal",
+            "price":"116,000",
+            "ingrediants":"tomato sauce, mushroom, mozarella, bakon, salami",
+            "star": 5,
+            "imageAddress":"./static/img/pizza/royal.png"
+        }
+    ];
+    
+}   
+
+function displayNext() {
+
+
+}
